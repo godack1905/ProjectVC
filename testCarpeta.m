8c08a15b6e3869485c08ad366f19ecceeba830b0
ikerussss <iker.diaz.tellez@estudiantat.upc.edu>
2026-01-05 17:37:27 +0100

    fix

--- testCarpeta.m
+++ testCarpeta.m
@@ -17,7 +17,7 @@ descriptor_names = {
 load('trainedModel.mat');
 
 % Definir la carpeta amb les imatges de test
-test_folder = 'imatges_senyals\test\zona_cotxe';  % Canvia aquesta ruta si cal
+test_folder = 'imatges_senyals\test\no_soroll';  % Canvia aquesta ruta si cal
 
 % Obtenir totes les imatges (PNG, JPG, JPEG)
 png_files = dir(fullfile(test_folder, '*.png'));
