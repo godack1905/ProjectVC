d81e0c645d64449b2c7d91233d2c86d69383ecdd
ikerussss <iker.diaz.tellez@estudiantat.upc.edu>
2026-01-07 17:38:35 +0100

    entrega

--- testCarpeta.m
+++ testCarpeta.m
@@ -17,7 +17,7 @@ descriptor_names = {
 % Carregar el model entrenat
 load('trainedModel.mat');
 
-test_folder = 'imatges_senyals\test\vianant';
+test_folder = 'imatges_senyals\test\stop';
 
 % Obtenir totes les imatges (PNG, JPG, JPEG)
 png_files = dir(fullfile(test_folder, '*.png'));
